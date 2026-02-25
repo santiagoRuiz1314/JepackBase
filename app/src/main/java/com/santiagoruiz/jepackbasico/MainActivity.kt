f6b50cdef7a050ec60a4d9e13a05043dce49c12e
Santiago Ruiz <sruiz557@unab.edu.co>
2026-02-25 11:35:52 -0500

    Segunda Actividad

--- app/src/main/java/com/santiagoruiz/jepackbasico/MainActivity.kt
+++ app/src/main/java/com/santiagoruiz/jepackbasico/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -84,14 +86,14 @@ fun CompletedScreen(){
         verticalArrangement = Arrangement.Center
     ) {
 
-        // Imagen del centro
+
         Image(
             painter = painterResource(id = R.drawable.Test),
             contentDescription = "imagen completado",
             modifier = Modifier.size(200.dp)
         )
 
-        // Primer texto (ajustado)
+
         Text(
             text = "All tasks completed",
             fontWeight = FontWeight.Bold,
@@ -99,10 +101,136 @@ fun CompletedScreen(){
                 .padding(top = 24.dp, bottom = 8.dp)
         )
 
-        // Segundo texto (ajustado)
+
         Text(
             text = "Nice work!",
             fontSize = 16.sp
         )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun QuadrantsScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFEADDFF))
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = "Text composable",
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                Text(
+                    text = "Displays text and follows the recommended Material Design guidelines.",
+                    textAlign = TextAlign.Justify,
+                    fontSize = 16.sp
+                )
+            }
+
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFD0BCFF))
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = "Image composable",
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                Text(
+                    text = "Creates a composable that lays out and draws a given Painter class object.",
+                    textAlign = TextAlign.Justify,
+                    fontSize = 16.sp
+                )
+            }
+        }
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFB69DF8))
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = "Row composable",
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                Text(
+                    text = "A layout composable that places its children in a horizontal sequence.",
+                    textAlign = TextAlign.Justify,
+                    fontSize = 16.sp
+                )
+            }
+
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFF6EDFF))
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = "Column composable",
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                Text(
+                    text = "A layout composable that places its children in a vertical sequence.",
+                    textAlign = TextAlign.Justify,
+                    fontSize = 16.sp
+                )
+            }
+        }
     }
 }
