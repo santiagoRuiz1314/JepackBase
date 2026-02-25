c64533474d36106aab391bdeedb0d3ef183c45dc
Santiago Ruiz <sruiz557@unab.edu.co>
2026-02-25 11:26:06 -0500

    Segundo commit, actividad

--- app/src/main/java/com/santiagoruiz/jepackbasico/MainActivity.kt
+++ app/src/main/java/com/santiagoruiz/jepackbasico/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -63,4 +70,39 @@ fun contentScreen(){
 
     }
 
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CompletedScreen(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        // Imagen del centro
+        Image(
+            painter = painterResource(id = R.drawable.Test),
+            contentDescription = "imagen completado",
+            modifier = Modifier.size(200.dp)
+        )
+
+        // Primer texto (ajustado)
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+
+        // Segundo texto (ajustado)
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp
+        )
+    }
 }
